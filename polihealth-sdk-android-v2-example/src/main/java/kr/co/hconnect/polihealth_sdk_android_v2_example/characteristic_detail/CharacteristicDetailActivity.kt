b6012232b70b5b2a6726a5f219ca3f6d76ecde9f
곽민우 <kmwdev@hconnect.co.kr>
2024-08-12 16:59:21 +0900

    HOP-3381 블루투스 Write 예제 추가

--- polihealth-sdk-android-v2-example/src/main/java/kr/co/hconnect/polihealth_sdk_android_v2_example/characteristic_detail/CharacteristicDetailActivity.kt
+++ polihealth-sdk-android-v2-example/src/main/java/kr/co/hconnect/polihealth_sdk_android_v2_example/characteristic_detail/CharacteristicDetailActivity.kt
@@ -5,6 +5,7 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.Toast
+import kr.co.hconnect.bluetooth_sdk_android.HCBle
 import kr.co.hconnect.polihealth_sdk_android.PoliBLE
 import kr.co.hconnect.polihealth_sdk_android_v2_example.R
 
@@ -17,6 +18,10 @@ class CharacteristicDetailActivity : AppCompatActivity() {
         val characteristicUuid: String? = intent.getStringExtra("CHARACTERISTIC_UUID")
         val tvCharacteristicUuid: TextView = findViewById(R.id.tv_characteristic_uuid)
         val btnNotify: Button = findViewById(R.id.btn_notify)
+        val btnWriteCall: Button = findViewById(R.id.btn_write_call)
+        val btnWriteSms: Button = findViewById(R.id.btn_write_sms)
+        val btnWriteCallEmpty: Button = findViewById(R.id.btn_write_call_empty)
+        val btnWriteKakao: Button = findViewById(R.id.btn_write_kakao)
 
         tvCharacteristicUuid.text = "Characteristic UUID: $characteristicUuid"
 
@@ -27,5 +32,23 @@ class CharacteristicDetailActivity : AppCompatActivity() {
                 setCharacteristicNotification(true)
             }
         }
+
+        btnWriteCall.setOnClickListener {
+            HCBle.writeCharacteristic("Call_12:21_02-123-1234_".toByteArray())
+        }
+
+        btnWriteSms.setOnClickListener {
+            HCBle.writeCharacteristic("Message_12:10_010-1234-1234_".toByteArray())
+        }
+
+        btnWriteCallEmpty.setOnClickListener {
+            HCBle.writeCharacteristic("MissedCall_12:34_043-123-1234_".toByteArray())
+        }
+
+        btnWriteKakao.setOnClickListener {
+            HCBle.writeCharacteristic("Talk_12:45_".toByteArray())
+        }
+
+
     }
 }
