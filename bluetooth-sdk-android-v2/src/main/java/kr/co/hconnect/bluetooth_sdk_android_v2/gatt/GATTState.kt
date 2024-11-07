08a310f86d76b11667e2df310ac17cb700f4b239
곽민우 <kmwdev@hconnect.co.kr>
2024-11-07 10:46:09 +0900

    Feat: 상태값 String으로 변환하는 기능 추가

--- bluetooth-sdk-android-v2/src/main/java/kr/co/hconnect/bluetooth_sdk_android_v2/gatt/GATTState.kt
+++ bluetooth-sdk-android-v2/src/main/java/kr/co/hconnect/bluetooth_sdk_android_v2/gatt/GATTState.kt
@@ -1,7 +1,16 @@
-package kr.co.hconnect.bluetooth_sdk_android.gatt
+package kr.co.hconnect.bluetooth_sdk_android_v2.gatt
 
 object GATTState {
     const val DISCONNECTED = 0
     const val CONNECTED = 1
     const val CONNECTING = 2
+
+    fun getStateDescription(state: Int): String {
+        return when (state) {
+            DISCONNECTED -> "DISCONNECTED"
+            CONNECTED -> "CONNECTED"
+            CONNECTING -> "CONNECTING"
+            else -> "UNKNOWN_STATE"
+        }
+    }
 }
--- polihealth-sdk-android-v2/src/main/java/kr/co/hconnect/polihealth_sdk_android_v2/BLEState.kt
+++ polihealth-sdk-android-v2/src/main/java/kr/co/hconnect/polihealth_sdk_android_v2/BLEState.kt
@@ -1,4 +1,4 @@
-package kr.co.hconnect.polihealth_sdk_android
+package kr.co.hconnect.polihealth_sdk_android_v2
 
 object BLEState {
     const val BOND_NONE = 10
@@ -35,6 +35,30 @@ object BLEState {
             bondState == BOND_BONDED && connectionState != STATE_CONNECTED -> CONNECTING_BONDING
             bondState != BOND_BONDED && connectionState == STATE_CONNECTED -> CONNECTING_BONDING
             else -> NOT_CONNECTED_BONDED
+        }
+    }
+
+    fun getStateString(state: Int): String {
+        return when (state) {
+            BOND_NONE -> "BOND_NONE"
+            BOND_BONDING -> "BOND_BONDING"
+            BOND_BONDED -> "BOND_BONDED"
+            STATE_DISCONNECTED -> "STATE_DISCONNECTED"
+            STATE_CONNECTING -> "STATE_CONNECTING"
+            STATE_CONNECTED -> "STATE_CONNECTED"
+            STATE_DISCONNECTING -> "STATE_DISCONNECTING"
+            GATT_SUCCESS -> "GATT_SUCCESS"
+            GATT_READ_NOT_PERMITTED -> "GATT_READ_NOT_PERMITTED"
+            GATT_WRITE_NOT_PERMITTED -> "GATT_WRITE_NOT_PERMITTED"
+            GATT_INSUFFICIENT_AUTHENTICATION -> "GATT_INSUFFICIENT_AUTHENTICATION"
+            GATT_REQUEST_NOT_SUPPORTED -> "GATT_REQUEST_NOT_SUPPORTED"
+            GATT_INSUFFICIENT_ENCRYPTION -> "GATT_INSUFFICIENT_ENCRYPTION"
+            GATT_INVALID_OFFSET -> "GATT_INVALID_OFFSET"
+            GATT_INSUFFICIENT_AUTHORIZATION -> "GATT_INSUFFICIENT_AUTHORIZATION"
+            GATT_INVALID_ATTRIBUTE_LENGTH -> "GATT_INVALID_ATTRIBUTE_LENGTH"
+            GATT_CONNECTION_CONGESTED -> "GATT_CONNECTION_CONGESTED"
+            GATT_FAILURE -> "GATT_FAILURE"
+            else -> "UNKNOWN_STATE"
         }
     }
 }
